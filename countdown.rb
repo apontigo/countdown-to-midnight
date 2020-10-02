815fe129e6fbcc559376d787a78c6b5c935febce
apontigo <PontigoAndres@gmail.com>
2020-10-01 23:21:10 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+countdown_to_midnight  = 10
+def countdown_with_sleep(countdown_to_midnight)
+    sleep(5)
+end
 
-def countdown
+def countdown(countdown_to_midnight)
+    while countdown_to_midnight > 0
+        puts "#{countdown_to_midnight} SECOND(S)!"
+        countdown_to_midnight -= 1
+    end
+    if countdown_to_midnight == 0
+        return "HAPPY NEW YEAR!"
+    end
 end
